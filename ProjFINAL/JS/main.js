ddbe7f29edc35f1b5413917c9187f98f463b35f4
Camila T G Ramos <127363345+CamilaTamires@users.noreply.github.com>
2023-09-29 17:58:28 -0300

    Add files via upload

---
+++ ProjFINAL/JS/main.js
@@ -0,0 +1,50 @@
+let didScroll = false;
+let paralaxTitles = document.querySelectorAll('.paralax-title');
+
+const scrollInProgress = () => {
+  didScroll = true
+}
+
+const raf = () => {
+  if (didScroll) {
+    paralaxTitles.forEach((element, index) => {
+      element.style.transform = "translateX(" + window.scrollY / 10 + "%)"
+    })
+    didScroll = false;
+  }
+  requestAnimationFrame(raf);
+}
+
+
+(function () {
+  "use strict";
+
+  var carousels = function () {
+    $(".owl-carousel1").owlCarousel({
+      loop: true,
+      center: true,
+      margin: 0,
+      responsiveClass: true,
+      nav: false,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false
+        },
+        680: {
+          items: 2,
+          nav: false,
+          loop: false
+        },
+        1000: {
+          items: 3,
+          nav: true
+        }
+      }
+    });
+  };
+
+  (function ($) {
+    carousels();
+  })(jQuery);
+})();
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# projetoF
+# ProjFINAL
+Travel Mate
